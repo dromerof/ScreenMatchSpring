ef1794230ad21051e34648518e2fac928eceb2ad
Daniel Romero <dandres508@gmail.com>
2024-10-28 09:43:08 -0500

    Inicio de ScreenMatchSpring

---
+++ src/test/java/com/aluracursos/ScreenMatchSpring/ScreenMatchSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.ScreenMatchSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenMatchSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b779f68de0fb518046dc51119be1f34a8e2c0a59
Daniel Romero <dandres508@gmail.com>
2024-11-08 10:09:29 -0500

    Relacion entre series y episodios bd

--- src/main/java/com/aluracursos/ScreenMatchSpring/model/Serie.java
+++ src/main/java/com/aluracursos/ScreenMatchSpring/model/Serie.java
@@ -22,7 +22,7 @@ public class Serie {
     private String actores;
     private String poster;
     private String sinopsis;
-    @Transient
+    @OneToMany(mappedBy = "serie")
     private List<Episodio> episodios;
 
     public Serie() {
